4e84ad93bdb3938fb6efe91dd04731ef9e80295a
Boomathi <boomathi2047@gmail.com>
2025-09-14 22:40:16 +0530

    Update problem4.py

--- problem4.py
+++ problem4.py
@@ -6,8 +6,8 @@ def primes_upto(n):
         for i in range(2, num):
             if num % i == 0:
                 break
-            else:
-                primes.append(num)
+        else:
+            primes.append(num)
     return primes
 
 print(primes_upto(10))
